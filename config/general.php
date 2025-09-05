87583b9bb9cae0783ccdd88d0b67abcfa83b855c
Thomas Deinhamer <code@thasmo.dev>
2025-09-05 22:36:24 +0200

    chore: set configuration via env variables

--- config/general.php
+++ config/general.php
@@ -3,20 +3,7 @@
 use craft\config\GeneralConfig;
 
 return GeneralConfig::create()
-	->defaultWeekStartDay(1)
-	->omitScriptNameInUrls()
-	->preloadSingles()
-	->preventUserEnumeration()
-	->headlessMode()
-	->allowUpdates(false)
-	->allowAdminChanges(false)
-	->runQueueAutomatically(false)
-	->sendPoweredByHeader(false)
-	->disallowRobots()
-	->useEmailAsUsername()
-	->cpTrigger(null)
 	->aliases([
 		'@webroot' => dirname(__DIR__) . '/web',
 	])
-	->resourceBasePath('@webroot/assets/')
 ;
