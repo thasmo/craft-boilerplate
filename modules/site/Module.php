5a794cebdc41ac59dc088ffc89b60f0356a37b40
Thomas Deinhamer <code@thasmo.dev>
2025-09-06 21:23:13 +0200

    feat: disable update widget if necessary

--- modules/site/Module.php
+++ modules/site/Module.php
@@ -4,26 +4,30 @@ namespace Site;
 
 use Craft;
 use craft\base\Event;
+use craft\events\RegisterComponentTypesEvent;
 use craft\events\RegisterCpNavItemsEvent;
+use craft\services\Dashboard;
 use craft\web\twig\variables\Cp;
 
 class Module extends \yii\base\Module
 {
 	public function init(): void
 	{
-		$this->unregisterPluginStoreNavigationItem();
-	}
-
-	private function unregisterPluginStoreNavigationItem(): void
-	{
 		if (Craft::$app->config->general->devMode) {
 			return;
 		}
 
-		if (Craft::$app->config->general->allowAdminChanges) {
-			return;
+		if (!Craft::$app->config->general->allowAdminChanges) {
+			$this->unregisterPluginStoreNavigationItem();
 		}
 
+		if (!Craft::$app->config->general->allowUpdates) {
+			$this->unregisterCraftUpdatesWidget();
+		}
+	}
+
+	private function unregisterPluginStoreNavigationItem(): void
+	{
 		Event::on(
 			Cp::class,
 			Cp::EVENT_REGISTER_CP_NAV_ITEMS,
@@ -36,6 +40,25 @@ class Module extends \yii\base\Module
 
 				if ($index !== false) {
 					unset($event->navItems[$index]);
+				}
+			}
+		);
+	}
+
+	private function unregisterCraftUpdatesWidget(): void
+	{
+		Event::on(
+			Dashboard::class,
+			Dashboard::EVENT_REGISTER_WIDGET_TYPES,
+			function (RegisterComponentTypesEvent $event) {
+				$index = array_search(
+					'craft\widgets\Updates',
+					$event->types,
+					true
+				);
+
+				if ($index !== false) {
+					unset($event->types[$index]);
 				}
 			}
 		);
