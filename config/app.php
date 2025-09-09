ae0ec8567566617791b680b7b534d8fcc1312f1c
Thomas Deinhamer <184284+thasmo@users.noreply.github.com>
2025-09-09 22:10:31 +0200

    chore: configure redis for cache, sessions and queues (#1)

--- config/app.php
+++ config/app.php
@@ -11,6 +11,42 @@ return [
 		'site',
 	],
 	'components' => [
+		'cache' => [
+			'class' => yii\redis\Cache::class,
+			'defaultDuration' => Craft::$app->config->general->cacheDuration,
+			'redis' => [
+				'hostname' => App::env('REDIS_HOSTNAME') ?: 'localhost',
+				'port' => App::env('REDIS_PORT') ?: 6379,
+				'password' => App::env('REDIS_PASSWORD') ?: null,
+				'database' => 0,
+			],
+		],
+		'session' => function() {
+			$config = craft\helpers\App::sessionConfig();
+
+			$config['class'] = yii\redis\Session::class;
+
+			$config['redis'] = [
+				'hostname' => App::env('REDIS_HOSTNAME') ?: 'localhost',
+				'port' => App::env('REDIS_PORT') ?: 6379,
+				'password' => App::env('REDIS_PASSWORD') ?: null,
+				'database' => 1,
+			];
+
+			return Craft::createObject($config);
+		},
+		'queue' => [
+			'proxyQueue' => [
+				'class' => yii\queue\redis\Queue::class,
+				'redis' => [
+					'hostname' => App::env('REDIS_HOSTNAME') ?: 'localhost',
+					'port' => App::env('REDIS_PORT') ?: 6379,
+					'password' => App::env('REDIS_PASSWORD') ?: null,
+					'database' => 2,
+				],
+			],
+			'channel' => 'queue',
+		],
 		'deprecator' => [
 			'throwExceptions' => App::parseBooleanEnv('$CRAFT_DEV_MODE') ?? false,
 		],
